6900a9ae67473a1adbaac2940ef9caec7ed0a395
Matias Fonseca <fonsecamatiasm@gmail.com>
2023-05-18 00:27:21 -0300

    Update user_mapper.go

--- user_mapper.go
+++ user_mapper.go
@@ -14,7 +14,7 @@ func connect() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb+srv://soda:soda@cluster0.hk79o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+		"<mongo_url>",
 	))
 	if err != nil { log.Fatal(err) }
 	return client
